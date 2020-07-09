69ff8953ddbecdeed85f4ab8733273b88aef3684
welcome <welcome@DESKTOP-36IJN45>
2020-07-09 23:48:44 +0530

    committing to branchx

---
+++ src/Examples/NewTest2.java
@@ -0,0 +1,12 @@
+package Examples;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("first test");
+	 System.out.println("second");
+  }
+}
